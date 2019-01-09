3f3c10696aa7c03b534a490eb40dc0b2a9f34bd9
parth <parthvarde50@gmail.com>
2019-01-09 17:59:40 +0530

    Added the TCP_Client_File and TCP_Client_File

--- PRV_Networking/src/prv_networking/UDP_Client_Array.java
+++ PRV_Networking/src/prv_networking/UDP_Client_Array.java
@@ -16,19 +16,49 @@ import java.util.*;
 public class UDP_Client_Array {
     public static void main(String[] args) throws Exception {
         DatagramSocket ds = new DatagramSocket(3333);
-        byte[] rd = new byte[1024];
-        byte[] sd = new byte[1024];
+        byte sd[] = new byte[1024];
+        byte rd[] = new byte[1024];
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Array length = ");
+        System.out.print("Enter Length of Array = ");
         int len = sc.nextInt();
+        int a[] = new int[len];
         System.out.print("Enter Elements of Array = ");
-        for(int i=0;i<len;i++)
+        for(int i=0; i<len; i++)
         {
-            sd[i] = sc.nextByte();
+            a[i] = sc.nextInt();
         }
-        InetAddress addr = InetAddress.getByName("localhost");
-        DatagramPacket dp = new DatagramPacket(sd, sd.length, addr, 3331);
+        
+        InetAddress add=InetAddress.getByName("localhost");
+        String str = Integer.toString(a.length);
+        sd = str.getBytes();
+        DatagramPacket dp = new DatagramPacket(sd, sd.length, add, 3331);
         ds.send(dp);
+        for(int i=0;i<a.length;i++)
+        {
+            str = Integer.toString(a[i]);
+            sd = str.getBytes();
+            dp = new DatagramPacket(sd, sd.length, add, 3331);
+            ds.send(dp);
+        }
+        
+        DatagramPacket dp1 = new DatagramPacket(rd, rd.length);
+        
+        int a1[]=new int[len];
+        System.out.println("Sorted array = ");
+        for (int i=0; i<len; i++) 
+        {
+            ds.receive(dp1);
+            String str1 = new String(dp1.getData());
+            str1 = str1.trim();
+            a1[i] = Integer.parseInt(str1);
+            System.out.println(a1[i]);
+        }
+        
+        ds.receive(dp1);
+        String s1 = new String(dp1.getData());
+        s1 = s1.trim();
+        int sum = Integer.parseInt(s1);
+        System.out.println("Sum of Array = " + sum);
         
         ds.close();
     }
--- PRV_Networking/src/prv_networking/UDP_Server_Array.java
+++ PRV_Networking/src/prv_networking/UDP_Server_Array.java
@@ -1,26 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prv_networking;
 
 import java.net.*;
 import java.util.*;
 
-/**
- *
- * @author parth
- */
 public class UDP_Server_Array {
     public static void main(String[] args) throws Exception {
         DatagramSocket ds = new DatagramSocket(3331);
-        byte[] rd = new byte[1024];
-        byte[] sd = new byte[1024];
+        byte rd[] = new byte[1024];
+        byte sd[] = new byte[1024];
         DatagramPacket dp = new DatagramPacket(rd, rd.length);
         ds.receive(dp);
-        rd = dp.getData();
-        System.out.println("Array = " + rd);
+        
+        String str = new String(dp.getData());
+        str = str.trim();
+        int n, sum = 0;
+        n = Integer.parseInt(str);
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) 
+        {
+            ds.receive(dp);
+            str = new String(dp.getData());
+            str = str.trim();
+            a[i] = Integer.parseInt(str);
+            sum = sum + a[i];
+        }
+        Arrays.sort(a);
+        
+        InetAddress add = InetAddress.getByName("localhost");
+        DatagramPacket dp1;
+        
+        
+        for(int i=0;i<a.length;i++)
+        {
+            String str1=Integer.toString(a[i]);
+            sd = str1.getBytes();
+            dp1 = new DatagramPacket(sd, sd.length, add, 3333);
+            ds.send(dp1);
+        }
+        
+        String s1 = Integer.toString(sum);
+        sd = s1.getBytes();
+        dp1 = new DatagramPacket(sd, sd.length, add, 3333);
+        ds.send(dp1);
         
         ds.close();
     }
