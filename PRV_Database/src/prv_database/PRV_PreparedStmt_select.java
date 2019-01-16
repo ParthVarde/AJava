0cd523fd78c795fba547273a82b9e47355ae7a46
ParthVarde <parthvarde50@gmail.com>
2019-01-16 12:52:58 +0530

    Changed PRV_PreparedStmt_select.

--- PRV_Database/src/prv_database/PRV_PreparedStmt_select.java
+++ PRV_Database/src/prv_database/PRV_PreparedStmt_select.java
@@ -12,17 +12,18 @@ public class PRV_PreparedStmt_select {
         conn = DriverManager.getConnection(url, "root", "root");
         if(conn!=null)
         {
-            String sel = "select * from student where id = '1'";
+            String sel = "select * from student where id = ?";
             pstmt = conn.prepareStatement(sel);
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter id of student = ");
             int id = sc.nextInt();
-            System.out.print("Enter name of student = ");
-            String name = sc.next();
             pstmt.setInt(1,id);
-            pstmt.setString(2,name);
-            int rows = pstmt.executeUpdate();
-            System.out.println("Rows affected = " + rows);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next())
+            {
+                System.out.println("Id = " + rs.getInt("id"));
+                System.out.println("Name = " + rs.getString("name"));
+            }
         }
 
     }
