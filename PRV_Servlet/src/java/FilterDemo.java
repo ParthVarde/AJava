d96a7880c7a5705583e8a65658e908ad847f617e
ParthVarde <parthvarde50@gmail.com>
2019-01-29 10:45:05 +0530

    Changed login.java and FilterDemo.java

--- PRV_Servlet/src/java/FilterDemo.java
+++ PRV_Servlet/src/java/FilterDemo.java
@@ -7,7 +7,11 @@ public class FilterDemo implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain f) throws ServletException, IOException {
         //PrintWriter out = res.getWriter();
         //out.println("Servlet not visible , Filter Visible");
-        f.doFilter(req, res);
+        String uname = req.getParameter("uname");
+        String pass = req.getParameter("pass");
+        if(uname.equals("admin") && pass.equals("admin")) {
+            f.doFilter(req, res);
+        }
     }
     public void destroy() {}
 }
--- PRV_Servlet/src/java/login.java
+++ PRV_Servlet/src/java/login.java
@@ -4,15 +4,7 @@ import javax.servlet.http.*;
 
 public class login extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        String uname = req.getParameter("uname");
-        String pass = req.getParameter("pass");
-        if(uname.equals("admin") && pass.equals("admin")) {
-            RequestDispatcher rd = req.getRequestDispatcher("wel");
-            rd.forward(req, res);
-        }
-        else {
-            RequestDispatcher rd=req.getRequestDispatcher("login1.html");
-            rd.include(req,res);
-        }
+        RequestDispatcher rd = req.getRequestDispatcher("wel");
+        rd.forward(req, res);
     }
 }
